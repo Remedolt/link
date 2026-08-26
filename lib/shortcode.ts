e3b275abb325ff6ad7e3c945c4f9bf817a4e0bd5
Remedolt <69905151+Remedolt@users.noreply.github.com>
2026-08-26 09:05:38 +0300

    1

--- lib/shortcode.ts
+++
@@ -1,6 +0,0 @@
-import { customAlphabet } from "nanoid";
-
-// Unambiguous alphabet (no 0/O/1/l/I) for clean, readable, shareable codes.
-const alphabet = "23456789abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ";
-
-export const generateShortCode = customAlphabet(alphabet, 6);
